3b2ac57081e3080942192a94210ad211756dec3f
Hidayat Oemar <35457462+hidayatoemar@users.noreply.github.com>
2026-08-21 18:35:49 +0700

    dt: apply accepted CODEX056 transfer overlay core index

--- SOURCE_SNAPSHOT/RESTORE_SERVICE/packages/core-d01/src/index.ts
+++ SOURCE_SNAPSHOT/RESTORE_SERVICE/packages/core-d01/src/index.ts
@@ -3,3 +3,4 @@ export * from "./admission.ts";
 export * from "./purpose-binding.ts";
 export * from "./formation.ts";
 export * from "./incident-relations.ts";
+export * from "./pre-ticket-admission.ts";
